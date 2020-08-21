82fa7f3b6881cbbd72ca82ff267e81c974bd336a
mgcarre <mathgb@gmail.com>
2020-08-21 13:15:13 +0200

    mise à jour calques

--- src/layers.ts
+++ src/layers.ts
@@ -30,5 +30,6 @@ export const OSMFranceHumanitarianLayer = tileLayer('https://{s}.tile.openstreet
     attribution: '&copy; ybon'
 });
 export const ESRIWorldImageryLayer = tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    maxZoom: 18
 });
--- src/overlays.ts
+++ src/overlays.ts
@@ -1,11 +1,15 @@
 import { tileLayer } from 'leaflet';
 
-export const StamenTonerOverlay = tileLayer('https://stamen-tiles-{s}.a.ssfastly.net/toner-hybrid/{z}/{x}/{y}{r}.png', {
+/* export const StamenTonerOverlay = tileLayer('https://stamen-tiles-{s}.a.ssfastly.net/toner-hybrid/{z}/{x}/{y}{r}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     subdomains: 'abcd',
     minZoom: 0,
     maxZoom: 20
-});
+}); */
+
+export const TransportOverlay = tileLayer('http://www.openptmap.org/tiles/${z}/${x}/${y}.png', {
+    opacity: 0.8
+})
 
 export const R500Overlay = tileLayer('https://{s}.tile.openstreetmap.fr/route500/{z}/{x}/{y}.png', {
     opacity: 0.7,
